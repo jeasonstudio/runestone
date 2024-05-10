f38a1a944da3b9894624125aba68bd11e33772ba
Jeason <me@cowpoke.cc>
2024-05-10 11:37:21 +0800

    chore: support cjs output

--- build.js
+++ build.js
@@ -1,13 +1,23 @@
 import esbuild from 'esbuild';
 import { wasmLoader } from 'esbuild-plugin-wasm';
 
-await esbuild.build({
-  bundle: true,
-  entryPoints: ['runestone/index.js'],
-  format: 'esm',
-  platform: 'browser',
-  outfile: 'runestone/bundle.js',
-  plugins: [wasmLoader({ mode: 'embedded' })],
-});
+await Promise.all([
+  esbuild.build({
+    bundle: true,
+    entryPoints: ['runestone/index.js'],
+    format: 'esm',
+    platform: 'browser',
+    outfile: 'runestone/bundle.js',
+    plugins: [wasmLoader({ mode: 'embedded' })],
+  }),
+  esbuild.build({
+    bundle: true,
+    entryPoints: ['runestone/cjs/index.js'],
+    format: 'cjs',
+    platform: 'node',
+    outfile: 'runestone/cjs/bundle.js',
+    plugins: [wasmLoader({ mode: 'embedded' })],
+  }),
+]);
 
 console.log('Build complete!');
